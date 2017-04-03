bc1b1224a77478e37d19dabf769bc74fb93bf088
Adam Mujtaba <AdamMujtaba@Gmail.com>
2017-04-03 13:22:46 +0300

    Update SSH.py
    fixing minor issues with the script

--- SSH.py
+++ SSH.py
@@ -15,7 +15,7 @@ SSH.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 for connect in host:
     try:
         print('connecting to ' + host)
-        SSH.connect(host, port=22, username='adam', password='adam')
+        SSH.connect(host, port=22, username=username, password=password)
     except paramiko.ssh_exception.NoValidConnectionsError:
         print('multiple connections were made, please set time delay to the script.')
     except paramiko.ssh_exception.AuthenticationException:
@@ -25,9 +25,9 @@ for connect in host:
     except Exception:
         print('something went wrong')
 
-with open(config_file, 'r') as output:
-    output_config = output.readlines()
-    for line in output_config:
-        shell_input, shell_output, shell_error = SSH.exec_command(line.replace('\n', ''))
-with open('Output.txt', 'w') as writer:
-    writer.write(shell_output.read())
+    with open(config_file, 'r') as output:
+        output_config = output.readlines()
+        for line in output_config:
+            shell_input, shell_output, shell_error = SSH.exec_command(output_config[line].replace('\n', ''))
+    with open('Output.txt', 'w') as writer:
+        writer.write(shell_output.read())
